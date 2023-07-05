f92b263669b0613d8ed76ee43218ca9bf167319d
Nutsa_Margvelashvili <nutsa_margvelashvili@epam.com>
2023-07-05 16:07:18 +0400

    new chat

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import io from 'socket.io-client'
+import {useState} from "react";
+import Chat from "./chat";
+
+const socket = io.connect("http://localhost:3001")
+function App() {
+    const [username, setUsername] = useState('')
+    const [room, setRoom] = useState('')
+    const [showChat, setShowChat] = useState(false)
+
+    const joinRoom = () => {
+       if(username !== '' && room !== ''){
+           socket.emit("join_room", room)
+           setShowChat(true)
+       }
+    }
+  return (
+    <div className="App">
+        {!showChat ? (
+        <div className="joinChatContainer">
+      <header className="App-header">
+        <p> <code>Hi, I'm Nutsa Margvelashvili</code>
+        </p>
+         <h3>Join A Chat</h3>
+          <input type="text" placeholder={"Nuka..."} onChange={e => setUsername(e.target.value)}/>
+          <input type="text" placeholder={"Room ID..."} onChange={e => setRoom(e.target.value)}/>
+          <button onClick={joinRoom}>Join A Room</button>
+
+      </header>
+        </div>) :
+            (<Chat socket={socket} room={room} username={username}/>)
+        }
+    </div>
+  );
+}
+
+export default App;
