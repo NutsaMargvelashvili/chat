847ffbf80e159f4a7da9d9b9b1960f2a4a7fb53f
Nutsa_Margvelashvili <nutsa_margvelashvili@epam.com>
2023-07-05 21:54:04 +0400

    feat(main): add scss

--- src/App.js
+++ src/App.js
@@ -1,56 +1,66 @@
-import './App.css';
-import io from 'socket.io-client'
-import {useEffect, useState} from "react";
+import "./App.scss";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
 import Chat from "./chat";
 import Header from "./Components/Header";
 
-
-const socket = io.connect("https://chat-nutsamargvelashvili.onrender.com")
+const socket = io.connect("https://chat-nutsamargvelashvili.onrender.com");
 
 function App() {
-    const [username, setUsername] = useState('')
-    const [room, setRoom] = useState('')
-    const [showChat, setShowChat] = useState(false)
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://chat-nutsamargvelashvili.onrender.com/hello');
-            const jsonData = await response.text();
-            console.log(jsonData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-
-    const joinRoom = () => {
-       if(username !== '' && room !== ''){
-           socket.emit("join_room", room)
-           setShowChat(true)
-       }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://chat-nutsamargvelashvili.onrender.com/hello"
+      );
+      const jsonData = await response.text();
+      console.log(jsonData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
+  };
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
   return (
     <div className="App">
-         <Header/>
+      <Header />
 
-        {!showChat ? (
+      {!showChat ? (
         <div className="joinChatContainer">
-      <header className="App-header">
-        <p> <code>Hi, I'm Nutsa Margvelashvili</code>
-        </p>
-         <h3>Join A Chat</h3>
-          <input type="text" placeholder={"Nuka..."} onChange={e => setUsername(e.target.value)}/>
-          <input type="text" placeholder={"Room ID..."} onChange={e => setRoom(e.target.value)}/>
-          <button onClick={joinRoom}>Join A Room</button>
-
-      </header>
-        </div>) :
-            (<Chat socket={socket} room={room} username={username}/>)
-        }
+          <header className="chat-wrapper">
+            <p>
+              {" "}
+              <code>Hi, I'm Nutsa Margvelashvili</code>
+            </p>
+            <h3>Join A Chat</h3>
+            <input
+              type="text"
+              placeholder={"Nuka..."}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder={"Room ID..."}
+              onChange={(e) => setRoom(e.target.value)}
+            />
+            <button onClick={joinRoom}>Join A Room</button>
+          </header>
+        </div>
+      ) : (
+        <Chat socket={socket} room={room} username={username} />
+      )}
     </div>
   );
 }
