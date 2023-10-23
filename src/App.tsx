3e1be7340382463e7f9cf60875f4b82f0aebb84b
Nutsa_Margvelashvili <nutsa_margvelashvili@epam.com>
2023-10-23 17:07:17 +0400

    fix(main): fixed chat layout

--- src/App.tsx
+++ src/App.tsx
@@ -68,97 +68,95 @@ function App() {
               path={"chat"}
               element={
                 <div className={"joinChatContainer"}>
+                  <Particles
+                    id="tsparticles"
+                    init={particlesInit}
+                    loaded={particlesLoaded}
+                    options={{
+                      background: {
+                        color: {
+                          value: "transparent",
+                        },
+                      },
+                      fpsLimit: 120,
+                      interactivity: {
+                        events: {
+                          // onClick: {
+                          //   enable: true,
+                          //   mode: "push",
+                          // },
+                          onHover: {
+                            enable: true,
+                            mode: "repulse",
+                          },
+                          resize: true,
+                        },
+                        modes: {
+                          push: {
+                            quantity: 4,
+                          },
+                          repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                          },
+                        },
+                      },
+                      particles: {
+                        color: {
+                          value: "#ffffff",
+                        },
+                        links: {
+                          color: "#10e956",
+                          distance: 150,
+                          enable: true,
+                          opacity: 0.5,
+                          width: 1,
+                        },
+                        collisions: {
+                          enable: true,
+                        },
+                        move: {
+                          direction: "none",
+                          enable: true,
+                          outModes: {
+                            default: "bounce",
+                          },
+                          random: false,
+                          speed: 0.6,
+                          straight: false,
+                        },
+                        number: {
+                          density: {
+                            enable: true,
+                            area: 800,
+                          },
+                          value: 80,
+                        },
+                        opacity: {
+                          value: 0.5,
+                        },
+                        shape: {
+                          type: "circle",
+                        },
+                        size: {
+                          value: { min: 1, max: 5 },
+                        },
+                      },
+                      detectRetina: true,
+                    }}
+                  />
                   {!showChat ? (
-                    <>
-                      <Particles
-                        id="tsparticles"
-                        init={particlesInit}
-                        loaded={particlesLoaded}
-                        options={{
-                          background: {
-                            color: {
-                              value: "transparent",
-                            },
-                          },
-                          fpsLimit: 120,
-                          interactivity: {
-                            events: {
-                              // onClick: {
-                              //   enable: true,
-                              //   mode: "push",
-                              // },
-                              onHover: {
-                                enable: true,
-                                mode: "repulse",
-                              },
-                              resize: true,
-                            },
-                            modes: {
-                              push: {
-                                quantity: 4,
-                              },
-                              repulse: {
-                                distance: 200,
-                                duration: 0.4,
-                              },
-                            },
-                          },
-                          particles: {
-                            color: {
-                              value: "#ffffff",
-                            },
-                            links: {
-                              color: "#10e956",
-                              distance: 150,
-                              enable: true,
-                              opacity: 0.5,
-                              width: 1,
-                            },
-                            collisions: {
-                              enable: true,
-                            },
-                            move: {
-                              direction: "none",
-                              enable: true,
-                              outModes: {
-                                default: "bounce",
-                              },
-                              random: false,
-                              speed: 0.6,
-                              straight: false,
-                            },
-                            number: {
-                              density: {
-                                enable: true,
-                                area: 800,
-                              },
-                              value: 80,
-                            },
-                            opacity: {
-                              value: 0.5,
-                            },
-                            shape: {
-                              type: "circle",
-                            },
-                            size: {
-                              value: { min: 1, max: 5 },
-                            },
-                          },
-                          detectRetina: true,
-                        }}
-                      />
-                      <div className={"chat-container"}>
-                        <Home />
-                        <Signin
-                          socket={socket}
-                          setShowChat={setShowChat}
-                          setRoom={setRoom}
-                          setUsername={setUsername}
-                          room={room}
-                          username={username}
-                        ></Signin>
-                      </div>
-                    </>
+                    <div className={"chat-container"}>
+                      <Home />
+                      <Signin
+                        socket={socket}
+                        setShowChat={setShowChat}
+                        setRoom={setRoom}
+                        setUsername={setUsername}
+                        room={room}
+                        username={username}
+                      ></Signin>
+                    </div>
                   ) : (
                     <div className={"chat-container"}>
                       <Home />
--- src/Components/AppLayout/index.tsx
+++ src/Components/AppLayout/index.tsx
@@ -1,18 +1,17 @@
-import React, { useCallback } from "react";
+import React from "react";
+import "./style.scss";
 import Header from "../Header";
-import Particles from "react-tsparticles";
-import Home from "../Home";
-import { Container, Engine } from "tsparticles-engine";
-import { loadFull } from "tsparticles";
 import { Outlet } from "react-router-dom";
+import Footer from "../Footer";
 
 function AppLayout() {
   return (
-    <div className="AppLayout">
+    <div className="app-layout">
       <Header />
       <div className="main">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
